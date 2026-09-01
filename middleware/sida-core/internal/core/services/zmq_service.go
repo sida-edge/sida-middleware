4518cd7c95254ad7d9a7abc7cf0399774b38daf4
ersaraujo <araujoersa@gmail.com>
2026-09-01 16:40:59 -0300

    Adjust zmq service to add subscriber socket

--- middleware/sida-core/internal/core/services/zmq_service.go
+++ middleware/sida-core/internal/core/services/zmq_service.go
@@ -9,26 +9,53 @@ import (
 	"sida-core/internal/core/domain"
 )
 
-type ZMQPublisher struct {
-	socket *zmq4.Socket
+type ZMQService struct {
+	pubSocket *zmq4.Socket
+	subSocket *zmq4.Socket
 }
 
-func NewZMQPublisher(tcpPath string) (*ZMQPublisher, error) {
-	socket, err := zmq4.NewSocket(zmq4.PUB)
+func NewZMQService(pubPath string, subPaths []string, topicFilter string) (*ZMQService, error) {
+	pubSocket, err := zmq4.NewSocket(zmq4.PUB)
 	if err != nil {
 		return nil, err
 	}
 
-	address := "tcp://" + tcpPath
-	if err := socket.Bind(address); err != nil {
+	pubAddress := "tcp://" + pubPath
+	if err := pubSocket.Bind(pubAddress); err != nil {
 		return nil, err
 	}
 
-	log.Printf("ZMQ Publisher rodando em %s", address)
-	return &ZMQPublisher{socket: socket}, nil
+	log.Printf("ZMQ Publisher rodando em %s", pubAddress)
+
+	subSocket, err := zmq4.NewSocket(zmq4.SUB)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, subPath := range subPaths {
+		subAddress := "tcp://" + subPath
+
+		if err := subSocket.Connect(subAddress); err != nil {
+			pubSocket.Close()
+			subSocket.Close()
+			return nil, err
+		}
+		log.Printf("ZMQ Subscriber conectado a %s", subAddress)
+	}
+
+	if err := subSocket.SetSubscribe(topicFilter); err != nil {
+		pubSocket.Close()
+		subSocket.Close()
+		return nil, err
+	}
+
+	return &ZMQService{
+		pubSocket: pubSocket, 
+		subSocket: subSocket,
+	}, nil
 }
 
-func (p *ZMQPublisher) PublishUpdate(manifest domain.Manifest) error {
+func (s *ZMQService) PublishUpdate(manifest domain.Manifest) error {
 	topic := "sida/manifest/" + manifest.GatewayID
 
 	manifestJSON, err := json.Marshal(manifest)
@@ -37,7 +64,7 @@ func (p *ZMQPublisher) PublishUpdate(manifest domain.Manifest) error {
 		return err
 	}
 	
-	_, err = p.socket.SendMessage(topic, string(manifestJSON))
+	_, err = s.pubSocket.SendMessage(topic, string(manifestJSON))
 	if err != nil {
 		log.Printf("Erro ao publicar no ZMQ: %v", err)
 		return err
@@ -47,6 +74,40 @@ func (p *ZMQPublisher) PublishUpdate(manifest domain.Manifest) error {
 	return nil
 }
 
-func (p *ZMQPublisher) Close() error {
-	return p.socket.Close()
+func (s *ZMQService) Listen() ([]domain.Telemetry, error) {
+	telemetries []domain.Telemetry
+	for {
+		msg, err := s.subSocket.RecvMessage(0)
+		if err != nil {
+			return err
+		}
+
+		if len(msg) < 2 {
+			log.Println("Mensagem ZMQ inválida recebida")
+			continue
+		}
+
+		topic := msg[0]
+		message := msg[1]
+	
+		var telemetry domain.Telemetry
+		if err := json.Unmarshal([]byte(message), &telemetry); err != nil {
+			log.Printf("Erro ao desserializar a telemetria: %v", err)
+			continue
+		}
+		telemetries = append(telemetries, telemetry)
+
+		log.Printf("Telemetria recebida no tópico %s: %+v", topic, telemetry)
+
+	}
+	return telemetries, nil
+}
+
+func (c *ZMQClient) Close() error {
+	if err := c.pubSocket.Close(); err != nil {
+		c.subSocket.Close()
+		return err
+	}
+
+	return nil
 }
